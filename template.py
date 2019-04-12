bfe61d205c26d5e6a71a8da7a3d1d1a1d7196224
Kailun <likailun@tju.edu.cn>
2019-04-12 02:53:26 -0400

    added more analysis pipelines

--- template.py
+++ template.py
@@ -17,50 +17,154 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 ###################################################################################################################################################
 
+# storing format: <(0)movie_id, (1)title, (2)release year, (3)duration, (4)genre, (5)director id, (6)director, 
+# (7)main actor1, (8)main actor2, (9)buy price, (10)rent price, (11)amount, (12)purchase method, (13)user name, (14)transaction time>
 
 def run(args, pipeline_args):
     # INSERT YOUR CODE HERE
+    key_field_index = 0
+    if args.director_copies_sold or args.director_dollars_sold:
+        key_field_index = 5
+
     def SplitLine(line):
         # split to extract each field in the .csv file
-        line_modified = line.replace(', ', '_');
+        line_modified = line.replace(', ', '_')
         return line_modified.split(',')
 
 
     def PairWithCopies(fields):
-        movie_id = fields[0]
+        id = fields[key_field_index]
         purchase_method = fields[12]
         amount = fields[11]
-        return (movie_id, (amount if purchase_method == 'buy' else 0, amount if purchase_method == 'rent' else 0))
+        return (id, (amount if purchase_method == 'buy' else 0, amount if purchase_method == 'rent' else 0))
+
+
+    def PairWithRevenue(fields):
+        id = fields[key_field_index]
+        revenue = int(fields[9]) if fields[12] == 'buy' else int(fields[10])
+        return (id, revenue)
+
+
+    def PairWithTransaction(fields):
+        movie_id = fields[0]
+        user_name = fields[13]
+        date_time = fields[14]
+        return ((user_name, date_time), movie_id)
 
 
     def Sum(group):
         from operator import add
         buy_tot = 0
         rent_tot = 0
-        (movie_id, records) = group
+        (id, records) = group
         for record in records:
             (buy_amt, rent_amt) = record
             buy_tot = buy_tot + int(buy_amt)
             rent_tot = rent_tot + int(rent_amt)
-        return (movie_id, buy_tot, rent_tot)
+        return (id, buy_tot, rent_tot)
 
 
-    def FormatResult(result):
-        (movie_id, buy_amt, rent_amt) = result
-        return '%s,%s,%s'% (movie_id, buy_amt, rent_amt)
+    def Permute(transaction):
+        ((user_name, date_time), movie_list) = transaction
+        li = []
+        position = 0
+        for movie_id in movie_list:
+            if len(movie_list) > 1:
+                for movie_id_other in movie_list:
+                    if (movie_id_other != movie_id): li.append(((movie_id, movie_id_other), 1))
+            else:
+                li.append(((movie_id, None), 0))
+        return li
+
+
+    def ChangeKey(movie_combination):
+        #print(movie_combination)
+        (movie_id, movie_id_other), count = movie_combination
+        return (movie_id, (movie_id_other, count))
+
+
+    def Sort(movie_and_list):
+        from operator import itemgetter
+        (movie_id, purchased_together_tuples) = movie_and_list
+        highest_list = []
+        sorted_list = sorted(purchased_together_tuples, key=itemgetter(1), reverse=True)
+        if sorted_list[0][1] == 0:
+            highest_list.append(('None', str(0)))
+        else:
+            i = 0
+            while i < len(sorted_list) and sorted_list[i][1] == sorted_list[0][1]:
+                highest_list.append(sorted_list[i])
+                i = i + 1
+        return (movie_id, highest_list)
+
+
+    def FormatMovieNumbers(result):
+        (id, buy_tot, rent_tot) = result
+        return '%s\t%s\t%s'% (id, str(buy_tot), str(rent_tot))
+
+
+    def FormatMovieRevenue(result):
+        (id, revenue_tot) = result
+        return '%s\t%s'% (id, str(revenue_tot))
+
+
+    def FormatHighestList(result):
+        movie_id, highest_list = result
+        li = []
+        #print(movie_id)
+        li.append(str(movie_id))
+        for highest_movie in highest_list:
+            #print(highest_movie)
+            li.append(str(highest_movie[0]))
+        frequency = highest_list[0][1]
+        li.append(str(frequency))
+        result_formatted = '\t'.join(li)
+        return result_formatted
 
 
     with beam.Pipeline(options=PipelineOptions(pipeline_args)) as pipeline:
     	lines = pipeline | beam.io.ReadFromText(args.input)
-        output = (
+        fields = (
             lines
             | 'Split' >> beam.Map(SplitLine)
-            | 'KeyValuePairs' >> beam.Map(PairWithCopies)
-            | 'GroupAndSum' >> beam.GroupByKey()
-            | 'Merge' >> beam.Map(Sum)
-            | 'Format' >> beam.Map(FormatResult)
         )
-        output | beam.io.WriteToText(args.output)
+        filtered_fields = (
+            fields
+            | 'Filter' >> beam.Filter(lambda field: args.genre is None and field is not None or args.genre is not None and field[4] == args.genre)
+        )
+
+        if args.copies_sold or args.director_copies_sold:
+            movie_numbers = (
+                filtered_fields
+                | 'PairWithCopies' >> beam.Map(PairWithCopies)
+                | 'GroupAndSum' >> beam.GroupByKey()
+                | 'MergeAmount' >> beam.Map(Sum)
+                | 'FormatRenvenue' >> beam.Map(FormatMovieNumbers)
+            )
+            movie_numbers | 'WriteMovieNumbers' >> beam.io.WriteToText(args.output)
+
+        if args.dollars_sold or args.director_dollars_sold:
+            movie_revenue = (
+                filtered_fields
+                | 'PairWithRevenue' >> beam.Map(PairWithRevenue)
+                | 'CombineRevenue' >> beam.CombinePerKey(sum)
+                | 'FormatRevenue' >> beam.Map(FormatMovieRevenue)
+            )
+            movie_revenue | 'WriteMovieRevenue' >> beam.io.WriteToText(args.output)
+
+        if args.purchased_together:
+            highest_list = (
+                filtered_fields
+                | 'PairWithTrnasaction' >> beam.Map(PairWithTransaction) # (user_name, date_time), movie_id
+                | 'GroupByTransaction' >> beam.GroupByKey()
+                | 'Permute' >> beam.FlatMap(Permute) # (movie_id, movie_id_other), 1
+                | 'CombineMovieCombo' >> beam.CombinePerKey(sum)
+                | 'ChangeKey' >> beam.Map(ChangeKey) # movie_id, (movie_id_other, count)
+                | 'GroupByMovie' >> beam.GroupByKey() # movie_id, [(movie_id_other, count), ... ]
+                | 'SortList' >> beam.Map(Sort)
+                | 'FormatHighestList' >> beam.Map(FormatHighestList)
+            )
+            highest_list | 'WriteHighestList' >> beam.io.WriteToText(args.output)
 
     pass
 
